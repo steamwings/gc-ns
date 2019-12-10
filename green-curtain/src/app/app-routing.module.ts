2f51f152611df502b97f79fc338b7528cf48ed74
steamwings <steamwings@outlook.com>
2019-12-09 21:24:55 -0500

    Fix title, add web-only routing, and add android build config

--- green-curtain/src/app/app-routing.module.ts
+++ green-curtain/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { routes } from '@src/app/app.routes';
+import { HomeComponent } from './home/home.component';
+
+var webRoutes : Routes = [
+  {path: 'index', redirectTo:'home', pathMatch:'full'}
+]
+routes.forEach(r => webRoutes.push(r));
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(webRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
